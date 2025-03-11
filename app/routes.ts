373144d990c39c64cd5fec3df1417f0a157aaa89
Mark Anthony Llego <markllego@gmail.com>
2025-03-11 08:02:00 +0800

    Refactor route definitions to use the new route function for improved clarity

--- app/routes.ts
+++ app/routes.ts
@@ -1,15 +1,15 @@
 // app/routes.ts
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-  { path: "reviewer", file: "routes/quiz.tsx" },
-  { path: "results", file: "routes/results.tsx" },
-  { path: "flashcards", file: "routes/flashcards.tsx" },
-  { path: "deped-orders", file: "routes/deped-orders.tsx" },
-  { path: "study-notes", file: "routes/user-data.tsx" },
-  { path: "dashboard", file: "routes/dashboard.tsx" },
-  { path: "copyright", file: "routes/copyright.tsx" },
+  route("reviewer", "routes/quiz.tsx"),
+  route("results", "routes/results.tsx"),
+  route("flashcards", "routes/flashcards.tsx"),
+  route("deped-orders", "routes/deped-orders.tsx"),
+  route("study-notes", "routes/user-data.tsx"),
+  route("dashboard", "routes/dashboard.tsx"),
+  route("copyright", "routes/copyright.tsx"),
   // Catch-all route at the end to handle any non-matched paths
-  { path: "*", file: "routes/404.tsx" },
+  route("*", "routes/404.tsx"),
 ] satisfies RouteConfig;
